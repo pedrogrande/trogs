14a2e2fe4bfd4635c302e9dcebef7d0925f6c495
pedroargento <peter.argent@gmail.com>
2013-09-26 17:45:57 +1000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trogs::Application.config.secret_token = 'aa6e434a8ae67943ed113c03804e1e5211fa3b56caba5809d301cd4d813b1914ab8f2cd7fb1ae6d987cdbe5a7836b53083b866d4fcc4db2280ef7c73eea3860a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Trogs::Application.config.session_store :cookie_store, key: '_trogs_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Trogs::Application.config.session_store :active_record_store
